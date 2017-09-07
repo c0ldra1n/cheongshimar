ff238393ba39e6121f081900fef3c7f9d9746253
owenlee <super_bubble@naver.com>
2017-09-07 19:02:16 +0900

    pm0702

--- models/AREnroll.js
+++ models/AREnroll.js
@@ -5,7 +5,7 @@ var arenrollSchema = mongoose.Schema({
  arcurrent:{type:String, required:true},
  arar:{type:String, required:true},
  arroom:{type:String, required:true},
- arpurpose:{type:String, required:true},
+ arpurpose:{type:String},
  arname:{type:String, required:true},
  arsort:{type:String, required:true},
  arsex:{type:String, required:true},
