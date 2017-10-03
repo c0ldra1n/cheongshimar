ad2e6568fbd320b8d980667bd325e9635c8c8f1d
conquers <super_bubble@naver.com>
2017-10-04 00:35:57 +0900

    1004_0035

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ db.on("error", function(err){
  console.log("DB ERROR : ", err);
 });
 
-new cron.CronJob('35 10 * * *', function(){
+new cron.CronJob('0 37 10 * * *', function(){
   db.collection('openrolls').drop();
   db.collection('arenrolls').drop();
   console.log("현재 시각을 기준으로 DB가 초기화되었습니다.");
